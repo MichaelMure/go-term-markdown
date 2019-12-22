007a0bcf86278359f7de25d6d7dbdc7ffa030874
Michael Muré <batolettre@gmail.com>
2019-12-22 14:33:15 +0100

    fix unknown *ast.Math type by having explicit extension support

--- markdown.go
+++ markdown.go
@@ -12,9 +12,17 @@ import (
 )
 
 func Render(source string, lineWidth int, leftPad int, opts ...Options) []byte {
-	extensions := parser.CommonExtensions
+	extensions := parser.NoIntraEmphasis        // Ignore emphasis markers inside words
+	extensions |= parser.Tables                 // Parse tables
+	extensions |= parser.FencedCode             // Parse fenced code blocks
+	extensions |= parser.Autolink               // Detect embedded URLs that are not explicitly marked
+	extensions |= parser.Strikethrough          // Strikethrough text using ~~test~~
+	extensions |= parser.SpaceHeadings          // Be strict about prefix heading rules
+	extensions |= parser.HeadingIDs             // specify heading IDs  with {#id}
+	extensions |= parser.BackslashLineBreak     // Translate trailing backslashes into line breaks
+	extensions |= parser.DefinitionLists        // Parse definition lists
 	extensions |= parser.LaxHTMLBlocks          // more in HTMLBlock, less in HTMLSpan
-	extensions |= parser.NoEmptyLineBeforeBlock // no need for NL before a list
+	extensions |= parser.NoEmptyLineBeforeBlock // no need for new line before a list
 
 	p := parser.NewWithExtensions(extensions)
 	nodes := md.Parse([]byte(source), p)
