745873348e42656301fc26a2dab20689ae1f2a29
Michael Muré <batolettre@gmail.com>
2020-05-25 01:00:53 +0200

    fix test/example

--- fuzzing/fuzz.go
+++ fuzzing/fuzz.go
@@ -1,8 +1,15 @@
 package fuzzing
 
-import markdown "github.com/MichaelMure/go-term-markdown"
+import (
+	"io/ioutil"
+
+	markdown "github.com/MichaelMure/go-term-markdown"
+)
 
 func Fuzz(data []byte) int {
-	markdown.Render(string(data), 50, 4)
+	err := markdown.Render(ioutil.Discard, data, 50, 4)
+	if err != nil {
+		panic(err)
+	}
 	return 1
 }
